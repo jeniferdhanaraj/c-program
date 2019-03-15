bcdae7e5f526fd2be77f650c0074899f66723dfa
jeniferdhanaraj <47886982+jeniferdhanaraj@users.noreply.github.com>
2019-03-15 15:47:17 +0530

    Create bser1_1.c

---
+++ bser1_1.c
@@ -0,0 +1,20 @@
+void main()
+{
+int n;
+clrscr();
+printf("enter the no:);
+scanf("%d",&n);
+if(n==0)
+{
+printf("zero");
+}
+elseif(n<0)
+{
+printf("negative");
+}
+else
+{
+printf("positive");
+}
+getch();
+}
