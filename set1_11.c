c105e43eaa4725e93d7672c7a6e3e9f724d8c664
jeniferdhanaraj <47886982+jeniferdhanaraj@users.noreply.github.com>
2019-03-25 16:13:57 +0530

    Create set1_11.c

---
+++ set1_11.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[10];
+clrscr();
+printf("enter the day");
+scanf("%s",&a);
+if(a=='s')
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
